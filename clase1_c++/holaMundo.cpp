eee2e036c41d9dd3f5e2e7356cd349a78d8b9352
cabrademontes27 <cabrademontes@gmail.com>
2024-09-08 12:01:14 -0600

    .

---
+++ clase1_c++/holaMundo.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hola mundo \n" << endl;
+    cout << "ingresa 2 numeros" << endl;
+
+    int a,b,resultado;
+    
+
+    cin >> a;
+    cin >> b;
+    resultado = a + b;
+
+    cout << "el resultado es: " << resultado << endl;
+
+    return 0;
+}
